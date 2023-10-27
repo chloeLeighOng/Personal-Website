7db11a0f1e0dac5e9209dd098332e1c97838b49d
Chloe <chloeleighong@gmail.com>
2023-10-26 22:55:04 -0500

    Transfer Current Files
    Coming back from moving

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-const test = "Hi, I'm working!";
-
-console.log(test);
-
 const navHeader = document.querySelector('header');
 const scrollWatcher = document.createElement('div');
 
-scrollWatcher.setAttribute('data-scroll-watcher')
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+navHeader.before(scrollWatcher);
+
+navObserver = new IntersectionObserver(() => {
+    console.log(entries);
+    primaryHeader.classList.toggle('sticking')
+});
+
+navObserver.observe(scrollWatcher)
