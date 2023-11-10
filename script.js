483c16e1fffa3b9c5511108be1400ec253170c8a
Chloe <chloeleighong@gmail.com>
2023-11-10 16:03:22 -0600

    Making Nav bar change color

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// Watching for scroll
+const primaryHeader = document.querySelector(".primary-header");
+const scrollWatcher = document.createElement("div");
 
-const navHeader = document.querySelector('header');
-const scrollWatcher = document.createElement('div');
+scrollWatcher.setAttribute("data-scroll-watcher", "");
+primaryHeader.before(scrollWatcher);
 
-scrollWatcher.setAttribute('data-scroll-watcher', '');
-navHeader.before(scrollWatcher);
+const navObserver = new IntersectionObserver(
+  (entries) => {
+    primaryHeader.classList.toggle("sticking", !entries[0].isIntersecting);
+  },
+  { rootMargin: "50px 0px 0px 0px" }
+);
 
-navObserver = new IntersectionObserver(() => {
-    console.log(entries);
-    primaryHeader.classList.toggle('sticking')
-});
-
-navObserver.observe(scrollWatcher)
+navObserver.observe(scrollWatcher);
