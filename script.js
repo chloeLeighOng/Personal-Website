ce0c30ed78d9bb12e8331e68e7da326fe4309e55
Chloe <chloeleighong@gmail.com>
2023-11-28 17:11:07 -0600

    Nav bar
    More changes to nav bar - js integartion for scroll and watchers for page movement

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+
 // Watching for scroll
-const primaryHeader = document.querySelector(".primary-header");
+const header = document.querySelector("header");
 const scrollWatcher = document.createElement("div");
 
 scrollWatcher.setAttribute("data-scroll-watcher", "");
-primaryHeader.before(scrollWatcher);
+header.before(scrollWatcher);
 
 const navObserver = new IntersectionObserver(
   (entries) => {
-    primaryHeader.classList.toggle("sticking", !entries[0].isIntersecting);
+    header.classList.toggle("sticking", !entries[0].isIntersecting);
   },
   { rootMargin: "50px 0px 0px 0px" }
 );
 
 navObserver.observe(scrollWatcher);
+
+
+// Watches for page change and changes active css styling on navigation list
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight) {
+      current = section.getAttribute("id");
+    } 
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
